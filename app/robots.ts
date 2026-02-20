b50829bcabf7bf9d195a6050aef7001d43c984a8
Roshith Krishna <roshithkrishnakm@gmail.com>
2026-02-20 09:59:46 +0530

    Update base URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://foss-cev.org';
+    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://foss.cev.ac.in';
 
     return {
         rules: {
